d303b6eeb6d40d3fa28ed7bc610d7bcc0296b49a
mk-sdet <openboxes@protonmail.com>
2021-10-06 02:04:30 -0400

    Add markets endpoint

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const axios = require("axios");
 
 const app = express();
 const router = express.Router();
@@ -9,6 +10,14 @@ router.get("/", (req, res) => {
     hello: "hi!"
   });
 });
+router.get("/ftx", async (req, res) => {
+    try {
+        const response = await axios.get('https://ftx.com/api/markets')
+        res.status(200).json(response.data)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+  });
 
 app.use(`/.netlify/functions/api`, router);
 
